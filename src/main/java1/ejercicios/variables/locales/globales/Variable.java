4da75feae8a0e8f8e9adc3bc4d8227f8e69988ef
USER <USER@DESKTOP-O8V4P81>
2021-09-06 20:09:25 -0500

    Taller variables locales y globales

---
+++ src/main/java1/ejercicios/variables/locales/globales/Variable.java
@@ -0,0 +1,39 @@
+package ejercicios.variables.locales.globales;
+
+public class Variable {
+	
+		static int variableEstatica;
+		int variableGlobal;
+		void Variable(int parametro) {
+			int variableLocal;
+	
+			
+		//LOCAL
+			
+		void Variable(int parametro) {
+		int variableLocal = new nextInt();		
+		System.out.println("");
+		System.out.println("");
+		
+		
+		//GLOBAL
+		
+		int variableGlobal;
+		void Variable() {
+		System.out.println("");
+		public class OtraClase {
+		void otroMetodo() {
+		
+			
+	    private String nombre;
+		private int edad;
+		public void comer() {
+	    this.nombre="Salo"
+	    String plato= "arroz";
+	    plato = "sopa";
+	    plato = "jugo";
+
+			
+	}
+		
+	
