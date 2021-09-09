cd41eed26c8f7ded44000a80939bfde4335765a1
SALOMÉ JIMÉNEZ <USER@DESKTOP-O8V4P81>
2021-09-08 20:29:41 -0500

    Taller clase

--- src/main/java1/ejercicios/nonacces/staticos/Paciente.java
+++ src/main/java1/ejercicios/nonacces/staticos/Paciente.java
@@ -1,5 +1,26 @@
 package ejercicios.nonacces.staticos;
 
 public class Paciente {
+	private String nombre;
+	private static int edad;
+	private static String nacionalidad;
+	private static int edadlimite = 65;
+	
+	public void registrar(String nombre) {
+		System.out.println("Se registra el paciente: " + nombre);
+		
+	}
+	
+	private static void consultarNacionalidad() {
+		System.out.println();
+		
+	}
+	
+	public static void consultaNacionalidadStatica() {
+		System.out.println("La nacionalidad de:" + nacionalidad);
+	}
+	
+	//Métodos SET y GET
+			
 
 }
