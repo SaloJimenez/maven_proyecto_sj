066c0965ba332a5c05a53656a2c68f5f135ceba8
SALOMÉ JIMÉNEZ <USER@DESKTOP-O8V4P81>
2021-09-15 21:01:36 -0500

    Clases Wrappers y conversiones

---
+++ src/main/java1/ejercicios/clases/wrappers/Main.java
@@ -0,0 +1,133 @@
+package ejercicios.clases.wrappers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	   //1. byte 
+		    byte datoByte = 2; //ptimitivo
+		    Byte datoByteWrapper = 3; //wrapper
+		    System.out.println("primitivo" + datoByte);
+		    System.out.println("wrapper" + datoByteWrapper);
+		    
+		  //Conversiones1
+		    byte datoByte1 = 6;
+		    Byte datoConvertido = datoByte1;
+		    System.out.println("Byte convertido" + datoConvertido);
+		    
+		  //Conversiones2
+		    Byte datoConvertido2 = 6;
+		    byte datoByte2 = datoConvertido2.byteValue();
+		   	System.out.println("byte convertido" + datoByte2);	
+		    
+		   	
+	   //2. short
+		    short datoShort = 1; //ptimitivo
+		    Short datoShortWrapper = 1; //wrapper
+		    System.out.println("primitivo" + datoShort);
+		    System.out.println("wrapper" + datoShortWrapper);
+		  
+		  //Conversiones1
+		    short datoShort1 = 4;
+		    Short shortConvertido = datoShort1;
+		    System.out.println("Short convertido" + shortConvertido);
+		  
+		  //Conversiones2
+		    Short datoCnvertido2 = 4;
+		    short datoShort2 = datoConvertido2.byteValue();
+		    System.out.println("short convertido" + datoShort2);
+		        		
+		    		
+	   //3. int
+		    int edad = 45; //ptimitivo
+		    Integer edadWrapper = 48; //wrapper
+		    System.out.println("primitivo" + edad);
+		    System.out.println("wrapper" + edadWrapper);
+		    
+		  //Conversiones1
+		    int edad1 = 45;
+		    Integer integerConvertido = edad1;
+		    System.out.println("Integer convertido" + integerConvertido);
+		  
+		  //Conversiones2
+		    Integer integerConvertido2 = 23;
+		    int edad2 = integerConvertido2.intValue();
+		    System.out.println("int convertido" + edad2);
+		   
+		    
+ 	   //4. long
+		    long valorGrande = 125344L; //ptimitivo
+		    Long valorGrandeWrapper = 123123L; //wrapper
+		    System.out.println("primitivo" + valorGrande);
+		    System.out.println("wrapper" + valorGrandeWrapper);
+		    
+		  //Conversiones1
+		    long valorGrande1 = 125344L;
+		    Long longConvertido = valorGrande1;
+		    System.out.println("Long convertido" + longConvertido);
+		  
+		  //Conversiones2
+		    Long lonConvertido2 = 125344L;
+		    long valorGrande2 = lonConvertido2.longValue();
+		    System.out.println("long convertido" + valorGrande2);
+		  
+		    
+	   //5. boolean  
+		    boolean datoBoolean = true; //ptimitivo
+		    Boolean datoBooleanWrapper = true; //wrapper
+		    System.out.println("primitivo" + datoBoolean);
+		    System.out.println("wrapper" + datoBooleanWrapper);
+		    
+		  //Conversiones1
+		    boolean datoBoolean1 = true;
+		    Boolean datoConvertido1 = datoBoolean1;
+		    System.out.println("Boolean convertido" + datoConvertido1);
+		    
+		  //Conversiones2
+		    Boolean datoConvertido2 = true;
+		    boolean datoBoolean2 = datoConvertido2.booleanValue();
+		   	System.out.println("boolean convertido" + datoBoolean2);	
+		    
+		  
+	   //6. float
+		    float datoFloat = 3; //primitivo
+		    Float datoFloatWrapper = 2; //wrapper
+		    System.out.println("primitivo" + datoFloat);
+		    System.out.println("wrapper" + datoFloatWrapper);
+		    
+		  //Conversiones1
+		    float datoFloat1 = 3;
+		    Float datoConvertido1 = datoFloat1;
+		    System.out.println("Float convertido" + datoConvertido1);
+		    
+		  //Conversiones2
+		    Float datoConvertido2 = 3;
+		    float datoFloat2 = datoConvertido2.floatValue();
+		   	System.out.println("float convertido" + datoFloat2);	
+		    
+		    
+	   //7. double
+		    double datoDouble = 1;
+		    Double datoDoubleWrapper = 2;
+		    System.out.println("primitivo" + datoDouble);
+		    System.out.println("wrapper" + datoDoubletWrapper);
+		    
+		  //Conversiones1
+		    double datoDouble1 = 1;
+		    Double datoConvertido1 = datoDouble1;
+		    System.out.println("Double convertido" + datoConvertido1);
+		    
+		  //Conversiones2
+		    Double datoConvertido2 = 1;
+		    double datoDouble2 = datoConvertido2.doubleValue();
+		   	System.out.println("double convertido" + datoDouble2);
+		    
+		    
+	   //8. char 
+		    char datoChar = 3
+		    Character
+		    
+	}
+
+}
