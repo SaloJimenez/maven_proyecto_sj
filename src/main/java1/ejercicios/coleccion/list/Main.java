e923cb8b2e21696a09fd522bf0bdebda5dde3f63
SALOMÉ JIMÉNEZ <USER@DESKTOP-O8V4P81>
2021-09-17 20:11:58 -0500

    Taller list.

---
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -0,0 +1,25 @@
+package ejercicios.coleccion.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer numero1 = 1;
+		Integer numero2 = 4;
+		Integer numero3 = 8;
+		Integer numero4 = 20;
+		
+		// TODO Auto-generated method stub
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
+		
+		int vector[] = new int [10];
+		
+	}
+
+}
