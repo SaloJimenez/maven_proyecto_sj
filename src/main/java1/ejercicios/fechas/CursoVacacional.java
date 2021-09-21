2dec5be3b488a35521f578a77cd7080ad4132e5f
USER <USER@DESKTOP-O8V4P81>
2021-09-20 19:34:20 -0500

    Taller

---
+++ src/main/java1/ejercicios/fechas/CursoVacacional.java
@@ -0,0 +1,16 @@
+package ejercicios.fechas;
+
+public class CursoVacacional {
+	
+	private String nombre;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	
+	public String getNombre() {
+		return nombre;
+		
+	}
+	
+	public void setNombre(String nombre {
+
+}
