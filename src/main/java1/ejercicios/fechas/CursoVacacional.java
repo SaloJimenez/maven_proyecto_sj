0afa79ffb468a3f8dbbe5693ab209b0f311c4bab
USER <USER@DESKTOP-O8V4P81>
2021-09-20 20:11:46 -0500

    Taller 22

--- src/main/java1/ejercicios/fechas/CursoVacacional.java
+++ src/main/java1/ejercicios/fechas/CursoVacacional.java
@@ -2,15 +2,25 @@ package ejercicios.fechas;
 
 public class CursoVacacional {
 	
+	private int tiempo;
 	private String nombre;
 	private LocalDate fechaInicio;
 	private LocalDate fechaFin;
+	private Instructor instructor;
 	
 	public String getNombre() {
-		return nombre;
-		
+		return nombre;	
 	}
 	
 	public void setNombre(String nombre {
+		this.nombre = nombre;	
+	}
+	
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+	
+	
+	
 
 }
---
+++ src/main/java1/ejercicios/fechas/Instructor.java
@@ -0,0 +1,25 @@
+package ejercicios.fechas;
+
+import java.time.LocalDate;
+
+public class Instructor {
+	
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private String direccionDomicilio;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(STring nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido( ) {
+		return apellido;
+	}
+
+}
